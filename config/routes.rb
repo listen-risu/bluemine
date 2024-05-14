99aacebfe5d21575ce4cac676506b87248a02bb4
listen-risu <listen.risu.blog@gmail.com>
2024-05-14 17:16:27 +0900

    ガントチャート画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/projects', to: 'projects#index'
   get 'calendars/show'
-  get 'gantts/show'
+  get '/issues/gantt', to: 'gantts#show'
   get '/issues', to: 'issues#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
